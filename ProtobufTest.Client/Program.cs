fba5ea03344b703756459dbf64b97c62b844e9f5
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2024-09-15 21:35:51 +0300

    Клиент с сервером grpc

---
+++ ProtobufTest.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создайте gRPC канал
+using Grpc.Net.Client;
+using ProtobufTest.Server.GrpcService;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7110");
+
+// Создайте клиента
+var client = new Greeter.GreeterClient(channel);
+
+// Создайте запрос
+var request = new HelloRequest { Name = "Alice" };
+
+// Отправьте запрос и получите ответ
+var reply = await client.SayHelloAsync(request);
+
+// Выведите ответ на консоль
+Console.WriteLine("Greeting: " + reply.Message);
